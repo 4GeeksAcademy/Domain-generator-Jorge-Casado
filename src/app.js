c6e1c017f33b4b3f3a2484ee53bb0a41d3a58d16
Jorge Casado <130782016+jorgecasadob@users.noreply.github.com>
2023-07-02 19:34:46 +0000

    Spaces fixed

--- src/app.js
+++ src/app.js
@@ -6,16 +6,16 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  const pronouns = ["the", "our"];
-  const adjs = ["great", "big"];
-  const nouns = ["jogger", "racoon"];
-  const extens = [".com", "us", ".io", ".es"];
+  const pronouns = ["the","our"];
+  const adjs = ["great","big"];
+  const nouns = ["jogger","racoon"];
+  const extens = [".com",".us",".io",".es"];
 
   for (let pronoun of pronouns) {
     for (let adj of adjs) {
       for (let noun of nouns) {
         for (let exten of extens) {
-          console.log(pronoun, adj, noun, exten);
+          console.log(pronoun,adj,noun,exten);
         }
       }
     }
